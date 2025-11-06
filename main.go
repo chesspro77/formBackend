c154120f4e76b6d7477b55f67c961931c3d2f23a
Mabruri101 <muhammadmabruri99@gmail.com>
2025-11-06 09:55:41 +0700

    adding build

--- main.go
+++ main.go
@@ -13,6 +13,6 @@ func main() {
 	http.HandleFunc("/download-excel", handler.DownloadBoxDepositExcel)
 	fs := http.FileServer(http.Dir("dist"))
 	go http.Handle("/", fs)
-	fmt.Println("server started at localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("server started at localhost:80")
+	http.ListenAndServe("0.0.0.0:80", nil)
 }
