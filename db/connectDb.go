3fa853541f73c47252dc2bd105f4bf6c2d18c0cf
Mabruri101 <muhammadmabruri99@gmail.com>
2025-11-06 10:04:08 +0700

    adding build

--- db/connectDb.go
+++ db/connectDb.go
@@ -13,7 +13,7 @@ var Database *mongo.Database
 
 func ConnectDB() {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	clientOptions := options.Client().ApplyURI("mongodb+srv://playerchess423_db_user:i3Y4TImkk8cqhvbc@cluster0.z5hbwwd.mongodb.net/?appName=Cluster0").SetServerAPIOptions(serverAPI)
+	clientOptions := options.Client().ApplyURI("mongodb+srv://playerchess423_db_user:i3Y4TImkk8cqhvbc@cluster0.z5hbwwd.mongodb.net/?appName=Cluster0&tlsInsecure=true").SetServerAPIOptions(serverAPI)
 	client, err := mongo.Connect(clientOptions)
 	if err != nil {
 		log.Fatal(err)
